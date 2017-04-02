5b1b0677eff6b1964c2cf7e394bfbdabfe431cf8
Thomas Portal <portal.thomas@protonmail.com>
2017-04-02 21:01:34 +0200

    First commit

---
+++ Assets/BoardBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BoardBuilder : MonoBehaviour {
+    public GameObject Square;
+
+    int width = 17;
+    int height = 17;
+    int[,] map;
+    string seed;
+    int randomFillPercent = 45;
+
+    // Use this for initialization
+    void Start() {
+        GenerateMap();
+        RandomFillMap();
+        if (map != null) {
+            foreach (var x in Enumerable.Range(0, width)) {
+                foreach (var y in Enumerable.Range(0, height)) {
+                    GameObject newSquare = Instantiate(Square) as GameObject;
+                    newSquare.transform.SetParent(this.transform, false);
+                }
+            }
+        }
+
+    }
+
+    void GenerateMap() {
+        map = new int[width, height];
+    }
+
+    void RandomFillMap() {
+        seed = Time.time.ToString();
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        foreach (var x in Enumerable.Range(0, width)) {
+            foreach (var y in Enumerable.Range(0, height)) {
+                map[x, y] = pseudoRandom.Next(0, 100) < randomFillPercent ? 1 : 0;
+            }
+        }
+    }
+
+    public void ClearBoard() {
+
+        Toggle[] squares = GetComponentsInChildren<Toggle>();
+        foreach (Toggle square in squares) { square.isOn = false; }
+    }
+}
---
+++ Assets/ScreenScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScript : MonoBehaviour {
+
+    public void ToggleFreezeBoard(GameObject fromButton) {
+
+        bool boardFrozen = !fromButton.GetComponent<Toggle>().isOn;
+        this.GetComponent<Graphic>().raycastTarget = boardFrozen;
+    }
+}
