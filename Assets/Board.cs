0feb2046b9f74fb463af178b5f385a2efa227f94
Thomas Portal <portal.thomas@protonmail.com>
2017-04-03 21:14:38 +0200

    Code cleanup

---
+++ Assets/Board.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour {
+
+    public Toggle BoardSquare;
+
+    int WIDTH = 17;
+    int HEIGHT = 17;
+
+    void Start() {
+
+        foreach (int x in Enumerable.Range(0, WIDTH * HEIGHT)) {
+            Toggle newSquare = Instantiate(BoardSquare);
+            newSquare.transform.SetParent(this.transform, false);
+        }
+    }
+}
---
+++ Assets/TurnStack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnStack : MonoBehaviour {
+
+    public GameObject Board;
+    public GameObject BoardFreezeOverlay;
+
+    public void ClearBoardMethod() {
+
+        Toggle[] squares = Board.GetComponentsInChildren<Toggle>();
+        foreach (Toggle square in squares) { square.isOn = false; }
+    }
+
+    public void ToggleFreezeBoard(Toggle fromButton) {
+
+        bool boardFrozen = !fromButton.isOn;
+        BoardFreezeOverlay.GetComponent<Graphic>().raycastTarget = boardFrozen;
+    }
+}
