8696beb45a3b117ec927557f0a58c773da941a29
Thomas Portal <portal.thomas@protonmail.com>
2017-04-04 22:26:09 +0200

    'Edges'

---
+++ Assets/BoardHorizOverlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardHorizOverlay : MonoBehaviour {
+
+    public Toggle BoardSquareHorizEdge;
+
+    int WIDTH = 17;
+    int HEIGHT = 16;
+
+    void Start() {
+
+        foreach (int x in Enumerable.Range(0, WIDTH * HEIGHT)) {
+            Toggle newSquare = Instantiate(BoardSquareHorizEdge);
+            newSquare.transform.SetParent(this.transform, false);
+        }
+    }
+}
---
+++ Assets/BoardVertOverlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardVertOverlay : MonoBehaviour {
+
+    public Toggle BoardSquareVertEdge;
+
+    int WIDTH = 16;
+    int HEIGHT = 17;
+
+    void Start() {
+
+        foreach (int x in Enumerable.Range(0, WIDTH * HEIGHT)) {
+            Toggle newSquare = Instantiate(BoardSquareVertEdge);
+            newSquare.transform.SetParent(this.transform, false);
+        }
+    }
+}
